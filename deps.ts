ae155def8deecfedba3078f53a127513a5b40a5b
uki00a <uki00a@gmail.com>
2020-03-28 20:17:17 +0900

    chore: add deps.ts

---
+++ deps.ts
@@ -0,0 +1,4 @@
+import * as typeorm from "https://denolib.com/denolib/typeorm@v0.2.23-rc1/mod.ts";
+import * as dinatra from "https://denopkg.com/syumai/dinatra@0.9.0/mod.ts";
+
+export { typeorm, dinatra };
